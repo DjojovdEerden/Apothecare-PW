f50c4c74819858c5d66abbdf46c0d5ccd96d929e
MikeValpoort <6037098@mborijnland.nl>
2025-04-02 13:47:02 +0200

    login register

---
+++ register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.register.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Welcome Back</h1>
+
+
+        <a href="login.php" id="btnRegisterLogin">Login</a>
+
+
+        <div class="input-styling">
+            <h3>Email</h3>
+            <input type="text" name="email" id="email" placeholder="Enter a valid email">
+
+            <h3>Password</h3>
+            <input type="text" name="password" id="password" placeholder="Create-password">
+        </div>
+
+
+
+        <input type="button" value="Create account" id="create-account">
+
+
+
+
+
+
+
+    </div><!-- end of container -->
+    <?php
+
+    ?>
+</body>
+
+</html>
