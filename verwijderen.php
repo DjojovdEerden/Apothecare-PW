90a7608e95cb626248cc0b4bb8d1719dd129a8cc
Sameeranimal <147604643+Sameeranimal@users.noreply.github.com>
2025-04-02 14:39:12 +0200

    Create: verwijderen.php

---
+++ verwijderen.php
@@ -0,0 +1,25 @@
+<?php
+require 'db_connect.php';
+
+// Controleren of er een ID is meegegeven
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    die("Geen klant-ID opgegeven.");
+}
+$id = intval($_GET['id']);
+
+// Klant verwijderen
+$sql = "DELETE FROM klanten WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    echo "<p>Klant succesvol verwijderd!</p>";
+} else {
+    echo "<p>Fout bij verwijderen: " . $conn->error . "</p>";
+}
+
+// Doorsturen naar klantbeheer na 2 seconden
+echo "<script>setTimeout(() => { window.location.href = 'klantbeheer.php'; }, 2000);</script>";
+?>
+
+<a href="body.php">Terug naar Klantbeheer</a>
